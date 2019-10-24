594e77f67335110cc5e90b28e0711fc3ce186f73
kuzma <kuzma.dariusz@gmail.com>
2019-10-24 09:02:20 +0200

    Add colors.py

--- main.py
+++ main.py
@@ -9,13 +9,6 @@ from create import button_maker, writing_text
 
 pygame.init()
 
-colors = {
-    'black': (0, 0, 0), 'blue': (0, 0, 200), 'brown': (153, 77, 0), 'dark_cream': (192, 187, 142), 'light_cream': (238, 232, 188),
-    'gold': (218,165,32),
-    'green': (57, 166, 6), 'grey': (100, 100, 100), 'orange':(255, 117, 26), 'red': (187, 19, 5), 'pure_red': (255, 0, 0),
-    'ultra_blue': (0, 0, 255), 'ultra_green': (0, 255, 0), 'white': (255, 255, 255), 'violet': (153, 51, 255), 'yellow': (224, 240, 28),
-}
-
 clock = pygame.time.Clock()
 FPS = 60
 

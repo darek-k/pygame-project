55fbfc86008db2e513704bbb52f5bdabf037732d
kuzma <kuzma.dariusz@gmail.com>
2019-10-25 10:30:08 +0200

    Put button_maker into module

--- create/__init__.py
+++ create/__init__.py
@@ -1,2 +1,3 @@
 from .colors import colors
 from .writing_text import writing_text
+from .button_maker import button_maker
--- main.py
+++ main.py
@@ -6,40 +6,13 @@ import sys
 import time
 
 from clock import clock, FPS
-from create import writing_text
+from create import writing_text, button_maker
 from create.colors import colors
 from display import display
 from player import PlayerEquipment
 
 pygame.init()
 
-
-
-# Function that makes text
-# def writing_text(font, font_size, text_input, color, x, y):
-#     my_font = pygame.font.SysFont(font, font_size)
-#     text = my_font.render(text_input, True, (colors[color]))
-#     display.blit(text, (x, y))
-
-# Function that makes interactive buttons
-def button_maker(x, y, w, h, color_on, color_off, font, font_size, text_input, text_color,
-                 transparent_on=True, transparent_off=True, text_on=True):
-    mouse_position = pygame.mouse.get_pos()
-
-    # Active button
-    if x + w > mouse_position[0] > x and y + h > mouse_position[1] > y:
-        pygame.draw.rect(display, (colors[color_on]), (x, y, w, h), transparent_on)
-        my_font = pygame.font.SysFont(font, font_size)
-        text = my_font.render(text_input, True, (colors[text_color]))
-        display.blit(text, (x + 1, y + 2))
-
-    # Non active button
-    else:
-        pygame.draw.rect(display, (colors[color_off]), (x, y, w, h), transparent_off)
-        if text_on:
-            my_font = pygame.font.SysFont(font, font_size)
-            text = my_font.render(text_input, True, (colors[text_color]))
-            display.blit(text, (x + 1, y + 2))
 
 # Function that deletes item from inventory
 def delete_item_from_inventory(index):
